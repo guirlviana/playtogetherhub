ce09521dde1a8f53782897a31e6e26308de6b485
guirlviana <guirlviana@gmail.com>
2024-04-04 19:41:21 -0300

    feat: games list

---
+++ backend/src/games.ts
@@ -0,0 +1,32 @@
+export const games = [
+  {
+    name: "Grand Theft Auto V",
+    externalCode: 1,
+    platforms: ["Xbox", "Playstation", "PC"],
+  },
+  {
+    name: "Minecraft",
+    externalCode: 2,
+    platforms: ["Xbox", "Playstation", "PC"],
+  },
+  {
+    name: "Sea of thieves",
+    externalCode: 3,
+    platforms: ["Xbox", "Playstation", "PC"],
+  },
+  {
+    name: "League of Legends",
+    externalCode: 4,
+    platforms: ["PC"],
+  },
+  {
+    name: "Counter Strike 2",
+    externalCode: 5,
+    platforms: ["PC"],
+  },
+  {
+    name: "World of warcraft",
+    externalCode: 6,
+    platforms: ["PC"],
+  },
+];
