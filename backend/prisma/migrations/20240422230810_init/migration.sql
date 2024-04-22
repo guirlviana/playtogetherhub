6583efe1070bc3536aa2703f3ae2aa617ebc629e
guirlviana <guirlviana@gmail.com>
2024-04-22 20:34:06 -0300

    update: db schema

--- backend/prisma/migrations/20240422230810_init/migration.sql
+++ backend/prisma/migrations/20240422230810_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "Gamer" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "email" TEXT NOT NULL,
+    "gamerTag" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "password" TEXT NOT NULL
 );
@@ -10,10 +10,9 @@ CREATE TABLE "Gamer" (
 CREATE TABLE "Games" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "externalCode" INTEGER NOT NULL,
-    "platform" INTEGER NOT NULL,
     "gamerId" INTEGER NOT NULL,
     CONSTRAINT "Games_gamerId_fkey" FOREIGN KEY ("gamerId") REFERENCES "Gamer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Gamer_email_key" ON "Gamer"("email");
+CREATE UNIQUE INDEX "Gamer_gamerTag_key" ON "Gamer"("gamerTag");
