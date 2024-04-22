596ca5ab186db0efcb43e93d34b4b56188faff65
guirlviana <guirlviana@gmail.com>
2024-04-22 20:34:48 -0300

    feat: route to create gamer

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import cors from "cors";
-
+import { createGamer, updateGamer } from "./usecases/gamer";
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!!");
+app.post("/create-gamer", async (req, res) => {
+  const data = req.body;
+  const { gamerTag, name, password } = data;
+
+  const gamer = await createGamer(gamerTag, name, password);
+
+  res
+    .status(200)
+    .json({ message: "created successfully", data: { gamerId: gamer.id } });
 });
 
 
