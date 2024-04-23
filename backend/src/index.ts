988ee10c28abff494857afe1d2030506525d79d9
guirlviana <guirlviana@gmail.com>
2024-04-23 19:39:01 -0300

    feat: create initial routes for games

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { router as gamerRoutes } from "./routes/gamer";
+import { router as gamesRoutes } from "./routes/games";
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/gamer", gamerRoutes);
+app.use("/games", gamesRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ backend/src/routes/games.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { createGameList, getGames } from "../usecases/games";
+
+export const router = Router();
+
+router.post("/create", async (req, res) => {
+  const { gamerId, games } = req.body;
+
+  const gamesUpdated = await createGameList(gamerId, games);
+
+  res.status(200).json({ message: "game list updated!", data: gamesUpdated });
+});
+
+router.get("/get/:gamerId", async (req, res) => {
+  const { gamerId } = req.params;
+
+  const games = await getGames(parseInt(gamerId));
+
+  res.status(200).json({ data: games });
+});
