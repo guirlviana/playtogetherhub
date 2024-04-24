10240726c06100a860e09922fb64116a1590fe29
guirlviana <guirlviana@gmail.com>
2024-04-24 19:47:54 -0300

    update: remove platforms from mocks

--- backend/src/common/games.ts
+++ backend/src/common/games.ts
@@ -4,31 +4,25 @@ export const games: Game[] = [
   {
     name: "Grand Theft Auto V (GTA)",
     externalCode: 1,
-    platforms: ["Xbox", "Playstation", "PC"],
   },
   {
     name: "Minecraft",
     externalCode: 2,
-    platforms: ["Xbox", "Playstation", "PC"],
   },
   {
     name: "Sea of thieves",
     externalCode: 3,
-    platforms: ["Xbox", "Playstation", "PC"],
   },
   {
     name: "League of Legends",
     externalCode: 4,
-    platforms: ["PC"],
   },
   {
     name: "Counter Strike 2 (CS)",
     externalCode: 5,
-    platforms: ["PC"],
   },
   {
     name: "World of warcraft",
     externalCode: 6,
-    platforms: ["PC"],
   },
 ];
--- backend/src/types/games.ts
+++ backend/src/types/games.ts
@@ -1,7 +1,6 @@
 export type Game = {
   name: string;
   readonly externalCode: number;
-  platforms: string[];
 };
 
 export type GameModel = {
