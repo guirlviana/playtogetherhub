26140eab545628af7ff260c9acad6145d2fc8810
guirlviana <guirlviana@gmail.com>
2024-04-24 20:42:25 -0300

    update: uses repository

--- backend/src/routes/games.ts
+++ backend/src/routes/games.ts
@@ -1,18 +1,12 @@
 import { Router } from "express";
-import {
-  createGamesList,
-  getAllGames,
-  getGames,
-  matchGames,
-  searchForGame,
-} from "../usecases/games";
+import { GamesRepository } from "../usecases/games";
 
 export const router = Router();
 
 router.post("/create", async (req, res) => {
   const { gamerId, games } = req.body;
 
-  const gamesUpdated = await createGamesList(gamerId, games);
+  const gamesUpdated = await GamesRepository.createGamesList(gamerId, games);
 
   res.status(200).json({ message: "game list updated!", data: gamesUpdated });
 });
@@ -20,7 +14,7 @@ router.post("/create", async (req, res) => {
 router.get("/get/:gamerId", async (req, res) => {
   const { gamerId } = req.params;
 
-  const games = await getGames(parseInt(gamerId));
+  const games = await GamesRepository.getGames(parseInt(gamerId));
 
   res.status(200).json({ data: games });
 });
@@ -28,21 +22,21 @@ router.get("/get/:gamerId", async (req, res) => {
 router.get("/match/:gamerId", async (req, res) => {
   const { gamerId } = req.params;
 
-  const match = await matchGames(parseInt(gamerId));
+  const match = await GamesRepository.matchGames(parseInt(gamerId));
 
   res.status(200).json({ data: match });
 });
 
 router.get("/all", (req, res) => {
   const { search } = req.query;
-  
+
   if (search) {
-    const games = searchForGame(search.toString());
+    const games = GamesRepository.searchForGame(search.toString());
     console.log(games);
     res.status(200).json({ data: games, nameSearched: search });
   }
-  
-  const games = getAllGames();
+
+  const games = GamesRepository.getAllGames();
 
   res.status(200).json({ data: games });
 });
