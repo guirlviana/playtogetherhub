1ff7cbbd19810e750501aacefbb58ace3fa94c73
guirlviana <guirlviana@gmail.com>
2024-04-25 21:53:20 -0300

    update: validate token provided

--- backend/src/middlewares/withAuth.ts
+++ backend/src/middlewares/withAuth.ts
@@ -1,9 +1,16 @@
+import "dotenv/config";
 import jwt from "jsonwebtoken";
 
 export function withAuth(req: { headers: any }, res: any, next: () => void) {
   const { token } = req.headers;
   if (!token) return res.status(401).json({ token: "No token provided" });
-    
-  console.log(token, "token");
-  next();
+
+  const secret = String(process.env.LOGIN_SECRET_KEY);
+
+  try {
+    jwt.verify(token, secret);
+    next();
+  } catch {
+    return res.status(401).json({ token: "Invalid token provided" });
+  }
 }
