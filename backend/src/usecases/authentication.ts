431f72a987b3f315e2e30ddab15b889cf2278356
guirlviana <guirlviana@gmail.com>
2024-04-25 21:28:56 -0300

    feat: login usecase

---
+++ backend/src/usecases/authentication.ts
@@ -0,0 +1,17 @@
+import { orm } from "../../prisma/prismaClient";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+export async function login(gamerTag: string, password: string) {
+  const gamer = await orm.gamer.findUnique({ where: { gamerTag: gamerTag } });
+  if (!gamer) return false;
+
+  const result = bcrypt.compareSync(password, gamer.password);
+  if (!result) return false;
+
+  const secret = String(process.env.LOGIN_SECRET_KEY);
+  const token = jwt.sign({ gamerTag }, secret, { expiresIn: "1h" });
+
+  return token;
+}
