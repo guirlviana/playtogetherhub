c4f45f106313987e45d529bd8aa9e428225ad2b6
guirlviana <guirlviana@gmail.com>
2024-05-01 11:09:27 -0300

    feat: add initial colors pallete

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2196f3",
+        secondary: "#b4b9c2",
+      },
+    },
   },
   plugins: [],
 };
