57ddeff7eee43aeddd8f04d2de75dfe92e5b1c53
guirlviana <guirlviana@gmail.com>
2024-05-03 19:26:38 -0300

    feat: use Title component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Button from "../Components/Button";
+import Title from "../Components/Title";
 
 function HomePage() {
   return (
     <div className="flex justify-center h-screen flex-col gap-7">
-      <h1 className="text-primary lg:text-9xl text-5xl flex">
-        PlayTogether hub
-      </h1>
+      <Title>PlayTogether hub</Title>
       <p className="text-black flex text-2xl pt-5">
         Say goodbye to solo gaming and hello to endless fun with PlayTogether
         hub. Discover fellow gamers who share your gaming tastes and preferences
