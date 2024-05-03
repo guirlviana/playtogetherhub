da549be49b04c35293e20faf9d0eec7e7538b5eb
guirlviana <guirlviana@gmail.com>
2024-05-03 17:59:21 -0300

    feat: home page struct

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function HomePage() {
-  return <></>;
+  return (
+    <div className="px-20 flex flex-col gap-7">
+      <h1 className="text-primary text-9xl flex pt-40">Play together hub</h1>
+      <p className="text-black flex text-2xl pt-5">
+        Say goodbye to solo gaming and hello to endless fun with
+        PlayTogetherHub. Discover fellow gamers who share your gaming tastes and
+        preferences
+      </p>
+      <div className="flex gap-6">
+        <button className="py-2 px-4 bg-primary text-white rounded-lg">Login</button>
+        <button className="p-2 px-4 bg-primary text-white rounded-lg">Sign up</button>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
