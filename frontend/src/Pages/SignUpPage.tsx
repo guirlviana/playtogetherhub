c627f424c03ce723eda8d190a95a23835b1128b5
guirlviana <guirlviana@gmail.com>
2024-05-06 19:23:11 -0300

    update: append title

---
+++ frontend/src/Pages/SignUpPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Title from "../Components/Title";
+
+function SignUpPage() {
+    return (
+        <Title>PlayTogether hub</Title>
+        
+    )
+}
+
+export default SignUpPage;
