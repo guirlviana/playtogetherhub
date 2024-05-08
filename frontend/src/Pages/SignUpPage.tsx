cb89e5e96343987a36d9ac63cd83ef303bd65403
guirlviana <guirlviana@gmail.com>
2024-05-07 21:25:12 -0300

    feat: gamerTag input and style fixes

--- frontend/src/Pages/SignUpPage.tsx
+++ frontend/src/Pages/SignUpPage.tsx
@@ -5,22 +5,35 @@ import InputWithLabel from "../Components/InputWIthLabel";
 function SignUpPage() {
   const [fields, setFieldsValue] = useState({
     name: "",
+    gamerTag: "",
   });
 
   return (
-    <>
+    <div className="flex flex-col h-full w-full">
       <Title>PlayTogether hub</Title>
-      <InputWithLabel
-        label={"Name"}
-        name={"name-input"}
-        onChange={(v) =>
-          setFieldsValue((prevState) => ({ ...prevState, name: v }))
-        }
-        value={fields.name}
-        direction="col"
-        required
-      />
-    </>
+      <form>
+        <InputWithLabel
+          label={"Name"}
+          name={"name-input"}
+          onChange={(v) =>
+            setFieldsValue((prevState) => ({ ...prevState, name: v }))
+          }
+          value={fields.name}
+          direction="col"
+          required
+        />
+        <InputWithLabel
+          label={"Gamertag"}
+          name={"gamertag-input"}
+          onChange={(v) =>
+            setFieldsValue((prevState) => ({ ...prevState, gamerTag: v }))
+          }
+          value={fields.gamerTag}
+          direction="col"
+          required
+        />
+      </form>
+    </div>
   );
 }
 
