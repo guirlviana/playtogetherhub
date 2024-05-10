993e4007c7756e5121c7832a1ec815fc312a77a3
guirlviana <guirlviana@gmail.com>
2024-05-10 20:42:18 -0300

    fix: wrong closed tag place

--- frontend/src/Pages/LoginPage.tsx
+++ frontend/src/Pages/LoginPage.tsx
@@ -14,35 +14,35 @@ function SignUpPage() {
     <Page>
       <div className="flex flex-col h-full w-full align-center items-center">
         <Title customStyle="pb-10">PlayTogether hub</Title>
+        <form className="flex flex-col gap-4 bg-secondary-100 rounded-xl p-5 md:w-1/4 xl:w-1/4">
+          <InputWithLabel
+            label={"Gamertag"}
+            name={"gamertag-input"}
+            onChange={(v) =>
+              setFieldsValue((prevState) => ({ ...prevState, gamerTag: v }))
+            }
+            value={fields.gamerTag}
+            direction="col"
+            required
+          />
+          <InputWithLabel
+            label={"Password"}
+            name={"password-input"}
+            onChange={(v) =>
+              setFieldsValue((prevState) => ({ ...prevState, password: v }))
+            }
+            value={fields.password}
+            direction="col"
+            type="password"
+            required
+          />
+          <div className="self-center">
+            <Button variant="default" size="small">
+              Login
+            </Button>
+          </div>
+        </form>
       </div>
-      <form className="flex flex-col gap-4 bg-secondary-100 rounded-xl p-5 md:w-1/4 xl:w-1/4">
-        <InputWithLabel
-          label={"Gamertag"}
-          name={"gamertag-input"}
-          onChange={(v) =>
-            setFieldsValue((prevState) => ({ ...prevState, gamerTag: v }))
-          }
-          value={fields.gamerTag}
-          direction="col"
-          required
-        />
-        <InputWithLabel
-          label={"Password"}
-          name={"password-input"}
-          onChange={(v) =>
-            setFieldsValue((prevState) => ({ ...prevState, password: v }))
-          }
-          value={fields.password}
-          direction="col"
-          type="password"
-          required
-        />
-        <div className="self-center">
-          <Button variant="default" size="small">
-            Login
-          </Button>
-        </div>
-      </form>
     </Page>
   );
 }
