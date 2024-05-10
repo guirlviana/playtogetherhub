062e13c5f785b283482c042238734aac92b8d153
guirlviana <guirlviana@gmail.com>
2024-05-10 18:56:52 -0300

    update: size button prop

--- frontend/src/Components/Button.tsx
+++ frontend/src/Components/Button.tsx
@@ -3,13 +3,20 @@ import React, { ReactNode } from "react";
 type Props = {
   children: ReactNode;
   variant: "default";
+  size: "small";
 };
 
-function Button({ children, variant }: Props) {
+function Button({ children, variant, size }: Props) {
   const variants = {
     default: "py-2 px-4 bg-primary hover:bg-primary-700 text-white rounded-lg",
   };
-  return <button className={variants[variant]}>{children}</button>;
+  const sizes = {
+    small: "w-24 h-12",
+  };
+
+  const combinedStyles = `${variants[variant]} ${sizes[size]}`;
+
+  return <button className={combinedStyles}>{children}</button>;
 }
 
 export default Button;
