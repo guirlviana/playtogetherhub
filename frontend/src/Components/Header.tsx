fce20dd2008e70f9bc0e1814fce08edf93f3d5e0
guirlviana <guirlviana@gmail.com>
2024-05-14 20:51:00 -0300

    feat: initial Header component

---
+++ frontend/src/Components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="flex-row justify-between">
+      <h4>PlayTogether hub</h4>
+      <div>
+        <img src="./user-logo.webp" width={50} height={50} alt="gamer logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
