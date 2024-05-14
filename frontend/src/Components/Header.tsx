c8e7bb7b3e6bbe61e174f81d11516521beffe6c0
guirlviana <guirlviana@gmail.com>
2024-05-14 20:55:07 -0300

    update: minor styles

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
-    <div className="flex flex-row justify-between">
-      <h4>PlayTogether hub</h4>
+    <div className="flex flex-row justify-between items-center">
+      <h4 className="text-primary flex w-auto lg:text-5xl md:text-3xl sm:text-3xl text-3xl">PlayTogether hub</h4>
       <div className="bg-white rounded-full">
         <img src="./user-logo.webp" width={50} height={50} alt="gamer logo" />
       </div>
