79d17ccf250bc00ca1a0214c970bc09ef0d1be51
guirlviana <guirlviana@gmail.com>
2024-05-15 20:15:42 -0300

    update: test overflow

--- frontend/src/Pages/MatchGamers.tsx
+++ frontend/src/Pages/MatchGamers.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "../Components/Header";
 import Page from "../Components/Page";
+import GamerOverviewItem from "../Components/GamerOverviewItem";
 
 function MatchGamersPage() {
   return (
@@ -10,7 +11,22 @@ function MatchGamersPage() {
         <p className="text-secondary-900 lg:text-4xl md:text-4xl, sm:text-3xl text-3xl">
           Let's play together!
         </p>
-        <div className="bg-white rounded-lg max-w-1/2 sm:w-2/3 sm:h-3/4 w-full h-full"></div>
+        <div className="bg-white rounded-lg max-w-2xl sm:w-2/3 sm:h-3/4 w-full h-4/6 overflow-auto">
+          <ul className="w-full">
+            <GamerOverviewItem type="primary"/>
+            <GamerOverviewItem type="secondary"/>
+            <GamerOverviewItem type="primary"/>
+            <GamerOverviewItem type="secondary"/>
+            <GamerOverviewItem type="primary"/>
+            <GamerOverviewItem type="secondary"/>
+            <GamerOverviewItem type="primary"/>
+            <GamerOverviewItem type="secondary"/>
+            <GamerOverviewItem type="primary"/>
+            <GamerOverviewItem type="secondary"/>
+            <GamerOverviewItem type="primary"/>
+            <GamerOverviewItem type="secondary"/>
+          </ul>
+        </div>
       </Page>
     </div>
   );
