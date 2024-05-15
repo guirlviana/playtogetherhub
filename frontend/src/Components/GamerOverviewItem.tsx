25f166cd0191e47a41f41ce59da173c6c467ac74
guirlviana <guirlviana@gmail.com>
2024-05-15 19:59:29 -0300

    update: more styles for mock

--- frontend/src/Components/GamerOverviewItem.tsx
+++ frontend/src/Components/GamerOverviewItem.tsx
@@ -2,12 +2,18 @@ import React from "react";
 
 function GamerOverviewItem() {
   return (
-    <li>
-      <img src="./user-logo" />
-      <h5>John Doe</h5>
-      <p>@GamerProJohn</p>
-      <div>
-        <img></img>
+    <li className="flex gap-3 self-center p-2">
+      <div className="bg-white rounded-full">
+        <img src="./user-logo.webp" width={50} height={50} alt="gamer logo" />
+      </div>
+      <div className="flex grow flex-col">
+        <h5>John Doe</h5>
+        <p>@GamerProJohn</p>
+      </div>
+      <div className="flex gap-1">
+        <img src="./game.webp" width={50} height={80} alt="game logo" />
+        <img src="./game.webp" width={50} height={80} alt="game logo" />
+        <img src="./game.webp" width={50} height={80} alt="game logo" />
       </div>
     </li>
   );
