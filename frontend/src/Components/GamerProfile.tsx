6cbdbe75fa57eacac4e3a7d7d0e51fca28ebe36f
guirlviana <guirlviana@gmail.com>
2024-05-14 21:11:22 -0300

    feat: put code into component

---
+++ frontend/src/Components/GamerProfile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GamerProfile() {
+  return (
+    <div className="bg-white rounded-full">
+      <img src="./user-logo.webp" width={50} height={50} alt="gamer logo" />
+    </div>
+  );
+}
+
+export default GamerProfile;
--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,10 +1,12 @@
+import GamerProfile from "./GamerProfile";
+
 function Header() {
   return (
     <div className="bg-secondary-200 flex flex-row justify-between items-center lg:py-5 lg:px-40 p-5">
-      <h4 className="text-primary flex w-auto lg:text-5xl md:text-3xl sm:text-3xl text-3xl">PlayTogether hub</h4>
-      <div className="bg-white rounded-full">
-        <img src="./user-logo.webp" width={50} height={50} alt="gamer logo" />
-      </div>
+      <h4 className="text-primary flex w-auto lg:text-5xl md:text-3xl sm:text-3xl text-3xl">
+        PlayTogether hub
+      </h4>
+      <GamerProfile />
     </div>
   );
 }
