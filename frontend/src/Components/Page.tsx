2877a7f97f2a728e378ff1db6e3de9b3d1bedfe9
guirlviana <guirlviana@gmail.com>
2024-05-15 19:31:41 -0300

    feat: customStyle prop

--- frontend/src/Components/Page.tsx
+++ frontend/src/Components/Page.tsx
@@ -2,10 +2,17 @@ import React, { ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
+  customStyle?: string;
 };
 
-function Page({ children }: Props) {
-  return <div className="lg:py-5 lg:px-40 p-5 bg-secondary absolute w-full h-full">{children}</div>;
+function Page({ children, customStyle }: Props) {
+  const defaultStyle =
+    "lg:py-5 lg:px-40 p-5 bg-secondary absolute w-full h-full";
+  const combinedStyles = customStyle
+    ? `${defaultStyle} ${customStyle}`
+    : defaultStyle;
+
+  return <div className={combinedStyles}>{children}</div>;
 }
 
 export default Page;
