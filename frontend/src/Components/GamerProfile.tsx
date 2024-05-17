7862bdf8f3fb6dc475413e1f49acd0ed296336f5
guirlviana <guirlviana@gmail.com>
2024-05-16 22:00:18 -0300

    feat: dropdown menu

--- frontend/src/Components/GamerProfile.tsx
+++ frontend/src/Components/GamerProfile.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function GamerProfile() {
+  const [isDropDownOpen, setIsDropdownOpen] = useState(false);
+
   return (
-    <div className="bg-white rounded-full">
-      <img src="./user-logo.webp" width={50} height={50} alt="gamer logo" />
+    <div className="inline-block relative">
+      <div
+        onClick={() => setIsDropdownOpen((prev) => !prev)}
+        className="bg-white rounded-full"
+      >
+        <img src="./user-logo.webp" width={50} height={50} alt="gamer logo" />
+      </div>
+      <div
+        className={`flex flex-col gap-1 w-max absolute z-10 float-left bg-white ${
+          isDropDownOpen ? "block" : "hidden"
+        }`}
+      >
+        <a href="/profile" className="text-gray-300 pr-5 p-1 text-1xl">Profile</a>
+        <a href="/gamerlist" className="text-gray-300 pr-5 p-1 text-1xl">Gamer list</a>
+        <a href="/logout" className="text-red-600 pr-5 p-1 text-1xl border-t">Log out</a>
+      </div>
     </div>
   );
 }
