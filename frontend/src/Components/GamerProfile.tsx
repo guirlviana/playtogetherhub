8f722fbd6693ea25e819d85a68cb97ebc1ecd6f7
guirlviana <guirlviana@gmail.com>
2024-05-17 22:41:06 -0300

    feat: dropdown show animation

--- frontend/src/Components/GamerProfile.tsx
+++ frontend/src/Components/GamerProfile.tsx
@@ -4,33 +4,21 @@ function GamerProfile() {
   const [isDropDownOpen, setIsDropdownOpen] = useState(false);
 
   return (
-    <div className="inline-block relative ">
+    <div className="inline-block relative">
       <div
         onClick={() => setIsDropdownOpen((prev) => !prev)}
-        className="bg-white rounded-full cursor-pointer"
+        className="bg-white rounded-full cursor-pointer relative"
       >
         <img src="./user-logo.webp" width={50} height={50} alt="gamer logo" />
         {isDropDownOpen && (
           <div
-            className={`flex flex-col gap-1 w-max right-6 absolute z-10 bg-white`}
+            className={`transition-all duration-300 ease-in-out origin-top-right transform absolute right-0 top-full mt-2 w-40 z-10 bg-white rounded-md shadow-lg animate-slide-down`}
           >
             <a
               href="/profile"
-              className="text-gray-300 hover:bg-primary-200 hover:text-white pr-5 p-1 text-1xl"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-primary-200 hover:text-white"
             >
               Profile
-            </a>
-            <a
-              href="/gamerlist"
-              className="text-gray-300 hover:bg-primary-200 hover:text-white pr-5 p-1 text-1xl"
-            >
-              Gamer list
-            </a>
-            <a
-              href="/logout"
-              className="text-red-600 hover:bg-red-600 hover:text-white pr-5 p-1 text-1xl border-t"
-            >
-              Log out
             </a>
           </div>
         )}
