89d697b7bf8f4ecb7887aa25ff8d83d3cd6c6b5d
guirlviana <guirlviana@gmail.com>
2024-05-17 22:42:07 -0300

    update: remove {}

--- frontend/src/Components/GamerProfile.tsx
+++ frontend/src/Components/GamerProfile.tsx
@@ -11,9 +11,7 @@ function GamerProfile() {
       >
         <img src="./user-logo.webp" width={50} height={50} alt="gamer logo" />
         {isDropDownOpen && (
-          <div
-            className={`transition-all duration-300 ease-in-out origin-top-right transform absolute right-0 top-full mt-2 w-40 z-10 bg-white rounded-md shadow-lg animate-slide-down`}
-          >
+          <div className="transition-all duration-300 ease-in-out origin-top-right transform absolute right-0 top-full mt-2 w-40 z-10 bg-white rounded-md shadow-lg animate-slide-down">
             <a
               href="/profile"
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-primary-200 hover:text-white"
