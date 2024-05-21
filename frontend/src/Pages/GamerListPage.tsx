83567dba42680e7d69f8f3c5aeea42d2b43a7e6e
guirlviana <guirlviana@gmail.com>
2024-05-20 21:09:52 -0300

    feat: initial structure

--- frontend/src/Pages/GamerListPage.tsx
+++ frontend/src/Pages/GamerListPage.tsx
@@ -1,7 +1,22 @@
 import Header from "../Components/Header";
+import Page from "../Components/Page";
 
 function GamerListPage() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Page>
+        <div className="flex flex-col gap-5">
+          <h3 className="text-2xl text-secondary-800">My games</h3>
+          <ul className="bg-secondary-200 p-6 rounded-md">
+            <li>
+              <img src="./game.webp" alt="game logo" width={150} height={210} />
+            </li>
+          </ul>
+        </div>
+      </Page>
+    </>
+  );
 }
 
 export default GamerListPage;
