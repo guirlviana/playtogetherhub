f34840aebb3cc784c6a6366140b572410b92a9b7
guirlviana <guirlviana@gmail.com>
2024-05-21 21:20:21 -0300

    update: redirect to home

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -3,9 +3,11 @@ import GamerProfile from "./GamerProfile";
 function Header() {
   return (
     <div className="bg-secondary-200 flex flex-row justify-between items-center lg:py-5 lg:px-40 p-5">
-      <h4 className="text-primary flex w-auto lg:text-5xl md:text-3xl sm:text-3xl text-3xl">
-        PlayTogether hub
-      </h4>
+      <a href="/">
+        <h4 className="text-primary flex w-auto lg:text-5xl md:text-3xl sm:text-3xl text-3xl">
+          PlayTogether hub
+        </h4>
+      </a>
       <GamerProfile />
     </div>
   );
