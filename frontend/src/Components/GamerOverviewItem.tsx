4a85890598cc0b298bc0c504fdb0659a776aca78
guirlviana <guirlviana@gmail.com>
2024-05-27 20:38:39 -0300

    update: append new prop values

--- frontend/src/Components/GamerOverviewItem.tsx
+++ frontend/src/Components/GamerOverviewItem.tsx
@@ -2,18 +2,21 @@ import React from "react";
 
 type Props = {
   type: "primary" | "secondary";
+  gamerTag: string;
+  name: string;
+  games: { externalCode: number }[];
 };
 
-function GamerOverviewItem({ type }: Props) {
-  const bgColor = type === "primary" ? "bg-white-100" : "bg-gray-50";
+function GamerOverviewItem(props: Props) {
+  const bgColor = props.type === "primary" ? "bg-white-100" : "bg-gray-50";
   return (
     <li className={`flex gap-3 p-2 ${bgColor}`}>
       <div className="bg-white rounded-full h-min flex">
         <img src="./user-logo.webp" width={50} height={50} alt="gamer logo" />
       </div>
       <div className="flex grow flex-col justify-center">
-        <h5>John Doe</h5>
-        <p>@GamerProJohn</p>
+        <h5>{props.name}</h5>
+        <p>{props.gamerTag}</p>
       </div>
       <div className="flex gap-1">
         <img src="./game.webp" width={50} height={80} alt="game logo" />
