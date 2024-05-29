07d6932719b86aba60d3f3f26abef2ba71631ab4
guirlviana <guirlviana@gmail.com>
2024-05-29 20:40:13 -0300

    update: headers and token

--- frontend/src/http/Games.tsx
+++ frontend/src/http/Games.tsx
@@ -16,10 +16,22 @@ export const matchFellowGamers = (gamerId: number, token: string | null) => {
 
 export const createOrUpdateGames = (
   gamerId: number,
-  games: { id: number; gamerId: number; externalCode: number }[]
+  games: { id: number; gamerId: number; externalCode: number }[],
+  token: string | null
 ) => {
-  return axios.post(BASE_URL + "/create-or-update", {
-    gamerId,
-    games,
-  });
+  return axios.post(
+    BASE_URL + "/create-or-update",
+    {
+      gamerId,
+      games,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+    }
+  );
 };
+
+export const getGamerList = (gamerId: number) => {};
