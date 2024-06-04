5e643e69f0511926c078a3e3a3f9e5936de1779e
guirlviana <guirlviana@gmail.com>
2024-06-04 18:56:48 -0300

    feat: save gamerId in session

--- backend/src/middlewares/withAuth.ts
+++ backend/src/middlewares/withAuth.ts
@@ -1,14 +1,19 @@
 import "dotenv/config";
 import jwt from "jsonwebtoken";
 
-export function withAuth(req: { headers: any }, res: any, next: () => void) {
+export function withAuth(
+  req: { headers: any; session?: { [key: string]: any } },
+  res: any,
+  next: () => void
+) {
   const { token } = req.headers;
   if (!token) return res.status(401).json({ token: "No token provided" });
 
   const secret = String(process.env.LOGIN_SECRET_KEY);
 
   try {
-    jwt.verify(token, secret);
+    const decoded = jwt.verify(token, secret) as { [key: string]: any };
+    req.session = { gamerId: decoded.gamerId };
     next();
   } catch {
     return res.status(401).json({ token: "Invalid token provided" });
