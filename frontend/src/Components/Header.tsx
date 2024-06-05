988566596dc46cada17c3aa877197dafc0e71c7b
guirlviana <guirlviana@gmail.com>
2024-06-05 18:41:38 -0300

    update: redirect to match

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -3,7 +3,7 @@ import GamerProfile from "./GamerProfile";
 function Header() {
   return (
     <div className="bg-secondary-200 flex flex-row justify-between items-center lg:py-5 lg:px-40 p-5">
-      <a href="/">
+      <a href="/match">
         <h4 className="text-primary flex w-auto lg:text-5xl md:text-3xl sm:text-3xl text-3xl">
           PlayTogether hub
         </h4>
