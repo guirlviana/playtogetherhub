7420c8d3fb809b83ede6dd8b807f2b55f9626ada
guirlviana <guirlviana@gmail.com>
2024-06-05 18:07:07 -0300

    update: remove headers

--- frontend/src/http/Gamer.tsx
+++ frontend/src/http/Gamer.tsx
@@ -13,46 +13,26 @@ export const createGamer = ({
   password,
   favoriteGameId,
 }: CreateGamer) => {
-  return httpService.post(
-    "/gamer/create",
-    {
-      gamerTag,
-      name,
-      password,
-      favoriteGameId,
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-};
-
-export const login = (gamerTag: string, password: string) => {
-  return httpService.post(
-    "/gamer/login",
-    {
-      gamerTag,
-      password,
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-};
-
-export const getAllGamers = () => {
-  return httpService.get("/gamer/all", {
-    headers: {
-      "Content-Type": "application/json",
-    },
+  return httpService.post("/gamer/create", {
+    gamerTag,
+    name,
+    password,
+    favoriteGameId,
   });
 };
 
-export const getGamer = (token: string | null) => {
+export const login = (gamerTag: string, password: string) => {
+  return httpService.post("/gamer/login", {
+    gamerTag,
+    password,
+  });
+};
+
+export const getAllGamers = () => {
+  return httpService.get("/gamer/all");
+};
+
+export const getGamer = () => {
   return httpService.get("/gamer/get");
 };
 
@@ -61,15 +41,6 @@ type editData = {
   gamerTag?: string;
 };
 
-export const editGamer = (data: editData, token: string | null) => {
-  return httpService.put(
-    "/gamer/update",
-    { ...data },
-    {
-      headers: {
-        "Content-Type": "application/json",
-        token: token,
-      },
-    }
-  );
+export const editGamer = (data: editData) => {
+  return httpService.put("/gamer/update", { ...data });
 };
