385df05647a86a8572aa9c4cc4071bacbfe2160d
guirlviana <guirlviana@gmail.com>
2024-06-06 21:08:59 -0300

    update: remove unset 404 route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,10 +9,6 @@ const PORT = 3001;
 app.use(express.json());
 app.use(cors());
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.status(404).json({ message: "Sorry can't find that!" });
-});
-
 app.use("/gamer", gamerRoutes);
 app.use("/games", gamesRoutes);
 
