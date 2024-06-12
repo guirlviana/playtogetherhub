7aa7ee045ca329b9ff7f11d305eb2c86e796fd8b
guirlviana <guirlviana@gmail.com>
2024-06-11 21:43:32 -0300

    update: validate name

--- frontend/src/Pages/SignUpPage.tsx
+++ frontend/src/Pages/SignUpPage.tsx
@@ -23,7 +23,11 @@ const validateFields = (fields: SignUpFields) => {
     );
     if (!isAllFieldsFilled) reject({ message: "Required fields not filled" });
 
-    if(fields.gamerTag.length > 20) reject({message: "GamerTag maximum 20 characters exceeded"})
+    if (fields.gamerTag.length > 20)
+      reject({ message: "GamerTag maximum 20 characters exceeded" });
+
+    if (fields.name.length > 20)
+      reject({ message: "Name maximum 20 characters exceeded" });
 
     resolve();
   });
