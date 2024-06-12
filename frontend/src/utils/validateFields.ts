e4991a789a21a3d893db4c08487b98522fac89d5
guirlviana <guirlviana@gmail.com>
2024-06-11 22:22:57 -0300

    feat: optionalvalue

--- frontend/src/utils/validateFields.ts
+++ frontend/src/utils/validateFields.ts
@@ -1,6 +1,6 @@
 type Fields = {
-  name: string;
-  gamerTag: string;
+  name?: string;
+  gamerTag?: string;
   password?: string;
   favoriteGameId?: string;
 };
@@ -11,11 +11,10 @@ export const validateFields = (fields: Fields) => {
       (value) => value.trim() !== ""
     );
     if (!isAllFieldsFilled) reject({ message: "Required fields not filled" });
-
-    if (fields.gamerTag.length > 20)
+    if (fields.gamerTag && fields.gamerTag.length > 20)
       reject({ message: "GamerTag maximum 20 characters exceeded" });
 
-    if (fields.name.length > 20)
+    if (fields.name && fields.name.length > 20)
       reject({ message: "Name maximum 20 characters exceeded" });
 
     resolve();
