7725e97eb1e89e8bc8eeaf6ae765bcb85a44ed38
guirlviana <guirlviana@gmail.com>
2024-06-11 22:00:19 -0300

    update: use util

--- frontend/src/Pages/SignUpPage.tsx
+++ frontend/src/Pages/SignUpPage.tsx
@@ -8,29 +8,13 @@ import { searchGames } from "../http/Games";
 import { createGamer } from "../http/Gamer";
 import { useNavigate } from "react-router-dom";
 import Error from "../Components/Error";
+import { validateFields } from "../utils/validateFields";
 
 type SignUpFields = {
   name: "";
   gamerTag: "";
   password: "";
   favoriteGameId: "";
-};
-
-const validateFields = (fields: SignUpFields) => {
-  return new Promise<void>((resolve, reject) => {
-    const isAllFieldsFilled = Object.values(fields).every(
-      (value) => value.trim() !== ""
-    );
-    if (!isAllFieldsFilled) reject({ message: "Required fields not filled" });
-
-    if (fields.gamerTag.length > 20)
-      reject({ message: "GamerTag maximum 20 characters exceeded" });
-
-    if (fields.name.length > 20)
-      reject({ message: "Name maximum 20 characters exceeded" });
-
-    resolve();
-  });
 };
 
 function SignUpPage() {
