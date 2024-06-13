8ce8084003fd331fdd4a708413227abdc86fe4a8
guirlviana <guirlviana@gmail.com>
2024-06-13 19:49:29 -0300

    update: change base_url

--- frontend/src/http/config.tsx
+++ frontend/src/http/config.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { logout } from "./Gamer";
 
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://playtogetherhub.onrender.com";
 
 export const httpService = axios.create({
   baseURL: BASE_URL,
