138d2240ffc2e14f17c61e9d0b504e078737900a
guirlviana <guirlviana@gmail.com>
2024-06-14 20:57:08 -0300

    feat: cors config for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
+import "dotenv/config";
 import cors from "cors";
 import { router as gamerRoutes } from "./routes/gamer";
 import { router as gamesRoutes } from "./routes/games";
@@ -7,7 +8,18 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.json());
-app.use(cors());
+
+const isProduction = parseInt(process.env.IS_PRODUCTION || "1");
+const corsProduction = {
+  origin:
+    "https://playtogetherhub-b8qn519xf-guilherme-vianas-projects-af351a31.vercel.app/",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+const corsConfig = isProduction ? corsProduction : {};
+app.use(cors(corsConfig));
 
 app.use("/gamer", gamerRoutes);
 app.use("/games", gamesRoutes);
