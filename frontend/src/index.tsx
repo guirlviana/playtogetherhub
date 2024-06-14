30858455304bff18bcb1b3531b73e4fa57d61517
guirlviana <guirlviana@gmail.com>
2024-06-14 20:43:26 -0300

    test

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,10 +4,10 @@ import "./index.css";
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import SignUpPage from "./Pages/SignUpPage";
-import HomePage from "./Pages/HomePage";
 import LoginPage from "./Pages/LoginPage";
 import MatchGamersPage from "./Pages/MatchGamersPage";
 import ProfilePage from "./Pages/ProfilePage";
+import HomePage from "./Pages/HomePage";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
