207b15a7d3270c7500d3f99ecc92e27125a827de
guirlviana <guirlviana@gmail.com>
2024-06-14 21:23:48 -0300

    update: specify only origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,9 +13,6 @@ const isProduction = parseInt(process.env.IS_PRODUCTION || "1");
 const corsProduction = {
   origin:
     "https://playtogetherhub-b8qn519xf-guilherme-vianas-projects-af351a31.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
 };
 
 const corsConfig = isProduction ? corsProduction : {};
