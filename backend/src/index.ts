210d98402ce6e1130255daf555e2504e3841fb80
guirlviana <guirlviana@gmail.com>
2024-06-14 21:00:09 -0300

    update: remove slash

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 const isProduction = parseInt(process.env.IS_PRODUCTION || "1");
 const corsProduction = {
   origin:
-    "https://playtogetherhub-b8qn519xf-guilherme-vianas-projects-af351a31.vercel.app/",
+    "https://playtogetherhub-b8qn519xf-guilherme-vianas-projects-af351a31.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
