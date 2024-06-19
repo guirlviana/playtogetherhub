7b59e8b1c3c3d71b908e512e6d624eb64fdfca1e
guirlviana <guirlviana@gmail.com>
2024-06-19 19:34:02 -0300

    feat: more games

--- backend/src/common/games.ts
+++ backend/src/common/games.ts
@@ -25,4 +25,9 @@ export const games: Game[] = [
     name: "World of warcraft",
     externalCode: 6,
   },
+  { name: "Fortnite", externalCode: 7 },
+  { name: "EA Sports FC 24", externalCode: 8 },
+  { name: "Call of Duty: Warzone", externalCode: 9 },
+  { name: "Rocket League", externalCode: 10 },
+  { name: "NBA 2K24", externalCode: 11 },
 ];
