6a9d1549499d025025c806de9384887a3899145e
guirlviana <guirlviana@gmail.com>
2024-06-18 21:26:43 -0300

    update: fallback message

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,5 +22,5 @@ app.use("/gamer", gamerRoutes);
 app.use("/games", gamesRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
