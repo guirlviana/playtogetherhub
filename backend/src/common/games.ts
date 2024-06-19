784e6bf5b88bd4deab95f8fd33cada7e60bd6f90
guirlviana <guirlviana@gmail.com>
2024-06-19 19:34:36 -0300

    update: format

--- backend/src/common/games.ts
+++ backend/src/common/games.ts
@@ -1,30 +1,12 @@
 import { Game } from "../types/games";
 
 export const games: Game[] = [
-  {
-    name: "Grand Theft Auto V (GTA)",
-    externalCode: 1,
-  },
-  {
-    name: "Minecraft",
-    externalCode: 2,
-  },
-  {
-    name: "Sea of thieves",
-    externalCode: 3,
-  },
-  {
-    name: "League of Legends",
-    externalCode: 4,
-  },
-  {
-    name: "Counter Strike 2 (CS)",
-    externalCode: 5,
-  },
-  {
-    name: "World of warcraft",
-    externalCode: 6,
-  },
+  { name: "Grand Theft Auto V (GTA)", externalCode: 1 },
+  { name: "Minecraft", externalCode: 2 },
+  { name: "Sea of thieves", externalCode: 3 },
+  { name: "League of Legends", externalCode: 4 },
+  { name: "Counter Strike 2 (CS)", externalCode: 5 },
+  { name: "World of warcraft", externalCode: 6 },
   { name: "Fortnite", externalCode: 7 },
   { name: "EA Sports FC 24", externalCode: 8 },
   { name: "Call of Duty: Warzone", externalCode: 9 },
