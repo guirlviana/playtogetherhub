fae0012973c030e1e52dcb6a8eafac28850aa957
guirlviana <guirlviana@gmail.com>
2024-07-29 21:32:31 -0300

    feat: isOnMaintenanceTime

---
+++ frontend/src/utils/maintenanceTime.ts
@@ -0,0 +1,9 @@
+function isOnMaintenanceTime() {
+    const now = new Date();
+    const currentHourUTC = now.getUTCHours();
+
+    const startHourUTC = 9;
+    const endHourUTC = 22;
+
+    return currentHourUTC < startHourUTC || currentHourUTC >= endHourUTC;
+}
