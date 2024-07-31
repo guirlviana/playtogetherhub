dec0508594117214f0356a31176ce70f73b99958
guirlviana <guirlviana@gmail.com>
2024-07-30 22:03:10 -0300

    update: never put maintenance time when is on development

--- frontend/src/utils/maintenanceTime.ts
+++ frontend/src/utils/maintenanceTime.ts
@@ -1,9 +1,13 @@
 export function isOnMaintenanceTime() {
-    const now = new Date();
-    const currentHourUTC = now.getUTCHours();
+  const ENVIROMENT = process.env.NODE_ENV;
+  const isDevelopmentEnviroment = ENVIROMENT === "development";
+  if (isDevelopmentEnviroment) return false;
 
-    const startHourUTC = 9;
-    const endHourUTC = 22;
+  const now = new Date();
+  const currentHourUTC = now.getUTCHours();
 
-    return currentHourUTC < startHourUTC || currentHourUTC >= endHourUTC;
+  const startHourUTC = 9;
+  const endHourUTC = 22;
+
+  return currentHourUTC < startHourUTC || currentHourUTC >= endHourUTC;
 }
